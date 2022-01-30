8e4f731136c10d6ed40f5374c9ba329e4b46a06d
Jayesh Bafna <53260266+Jayesh352002@users.noreply.github.com>
2022-01-29 22:54:29 -0800

    Add files via upload

--- Jayesh/Add To Cart/Cart.js
+++ Jayesh/Add To Cart/Cart.js
@@ -1,17 +1,62 @@
-var inp = document.getElementById('no').value;
-var p = document.getElementById('no');
-
-function sub()
+var i1 = document.getElementById('no').value;
+disable();
+function disable()
 {
-	//alert(inp)
-	var a = --inp;
-	p.value = a;
+	if(i1=="1")
+	{
+		//alert(i1);
+		document.getElementById('minus').disabled = true;
+	}
+
+}
+	
+function add()
+{
+	//alert("Hello Plus")
+	//alert(item)
+	var inp = document.getElementById('no').value;
+	var p = document.getElementById('no');
 	var total = document.getElementById('total-price').innerHTML;
 	var t = document.getElementById('total-price');
 	var item = document.getElementById('it-det').innerHTML;
 	var i = document.getElementById('it-det');
 	var rs = document.getElementById('r').innerHTML;
 	var rs1 = document.getElementById('r');
+	if(parseInt(inp)>=1)
+	{
+		//alert(parseInt(inp))
+		sub();
+	}
+	var a = ++inp;
+	p.value = a;
+	var b = parseInt(total)+70;
+	t.innerHTML = b ;
+	var c = parseInt(item)+1; 
+	i.innerHTML = c;
+	var d = parseInt(rs)+70;
+	rs1.innerHTML = d;
+	//alert(p.value)
+}
+
+function sub()
+{
+	//alert("Sub Called");
+	//var i1 = document.getElementById('no').value;
+	if(i1=="1")
+	{
+		disable();
+	}
+	document.getElementById('minus').disabled = false;
+	var inp = document.getElementById('no').value;
+	var p = document.getElementById('no');
+	var total = document.getElementById('total-price').innerHTML;
+	var t = document.getElementById('total-price');
+	var item = document.getElementById('it-det').innerHTML;
+	var i = document.getElementById('it-det');
+	var rs = document.getElementById('r').innerHTML;
+	var rs1 = document.getElementById('r');
+	var a = --inp;
+	p.value = a;
 	var b = parseInt(total)-70;
 	t.innerHTML = b ; 
 	var c = parseInt(item)-1; 
@@ -20,23 +65,26 @@ function sub()
 	rs1.innerHTML = d;
 	//alert(p.value)
 }
-function add()
+
+function save()
 {
-	//alert("Hello Plus")
-	//alert(item)
-	var a = inp++;
-	p.value = a;
-	var total = document.getElementById('total-price').innerHTML;
-	var t = document.getElementById('total-price');
-	var item = document.getElementById('it-det').innerHTML;
-	var i = document.getElementById('it-det');
-	var rs = document.getElementById('r').innerHTML;
-	var rs1 = document.getElementById('r');
-	var b = parseInt(total)+70;
-	t.innerHTML = b ;
-	var c = parseInt(item)+1; 
-	i.innerHTML = c;
-	var d = parseInt(rs)+70;
-	rs1.innerHTML = d;
-	//alert(p.value)
+	alert("Your Item is Save Successfully....")
+	//document.getElementById('med-img').src = "crocin.jpg";
+	//document.getElementById('save').style.color = "gray";
+}
+
+function open()
+{
+	//alert("Open Called")
+	window.open("https://www.w3schools.com");
+	var a1 = document.getElementById('crocin');
+	
+	//window.location = "Cart.html";
+	//imageLoad()
+}
+function imageLoad()
+{
+	//alert("ImageLoad Called")
+
+	document.getElementById('med-img').src = "crocin.jpg";
 }
