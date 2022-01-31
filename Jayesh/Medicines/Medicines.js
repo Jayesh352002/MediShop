d2972bf52db4c95c0dd97202488be7ceae4ab15f
Jayesh Bafna <53260266+Jayesh352002@users.noreply.github.com>
2022-01-31 05:40:11 -0800

    Add files via upload

---
+++ Jayesh/Medicines/Medicines.js
@@ -0,0 +1,14 @@
+function openCart(a)
+{
+	if(a=="calpol")
+	{
+		//alert("Calpol Tablet");
+		//document.getElementById('h4').innerHTML = "Calpol Tablet";
+		window.open("Cart.html");	
+	}
+	else
+	{
+		window.open("HomePage.html");
+	}
+	
+}
